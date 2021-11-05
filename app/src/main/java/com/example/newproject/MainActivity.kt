c61748fb4ab270e2701afaa3ef7bd4399d8d97b3
kveno <guram.kveniashvili.1@btu.edu.ge>
2021-11-05 22:10:13 +0400

    initial commit

--- app/src/main/java/com/example/newproject/MainActivity.kt
+++ app/src/main/java/com/example/newproject/MainActivity.kt
@@ -9,3 +9,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+class Person {
+    var name = ""
+    var age = 0
+    fun printName(){
+        println(name)
+    }
+
+    override fun toString(): String {
+        return "$name, $age"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if(other is Person){
+            if(name == other.name){
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+}
+fun main(){
+    var person1 = Person()
+    person1.name = "nika"
+    person1.age = 65
+    person1.printName()
+    var person2 = Person()
+    person2.name = "jemal"
+    person2.age = 84
+    println(person1)
+    println(person1 == person2)
+    println(person1.hashCode())
+    println(person2.hashCode())
+}
